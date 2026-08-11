adbebc9365ffd364b968dea9d16fa056ba05c3bc
helmesjo <helmesjo@gmail.com>
2026-08-11 23:48:36 +0200

    bdep new --package --lang c++,hxx=h,cxx=cpp --type lib,subdir=lodepng,third-party,auto-symexport,license=Zlib liblodepng

---
+++ liblodepng/lodepng/lodepng.cpp
@@ -0,0 +1,17 @@
+#include <lodepng/lodepng.h>
+
+#include <ostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace lodepng
+{
+  void say_hello (ostream& o, const string& n)
+  {
+    if (n.empty ())
+      throw invalid_argument ("empty name");
+
+    o << "Hello, " << n << '!' << endl;
+  }
+}
